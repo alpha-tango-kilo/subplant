0633adc98e0e856ec6f767d6f57ef6a58a554227
alpha-tango-kilo <git@heyatk.com>
2024-06-04 18:34:26 +0100

    Improve extract
    Handle no season/episode
    Extract subs all in one go
    Simplify sub_map within process (can't remember why I wanted the more complex thing)

--- src/subplant/extract.py
+++ src/subplant/extract.py
@@ -30,16 +30,20 @@ def guess_season_episode_from(file_name: str) -> tuple[int, int] | None:
 
 def process(mkv_path: Path, root_output_dir: Path) -> None:
     mkv = MKVFile(mkv_path)
-    # TODO: handle no season/episode info
-    season, episode = guess_season_episode_from(mkv_path.stem)
+    if tup := guess_season_episode_from(mkv_path.stem):
+        season, episode = tup
+    else:
+        season = None
+        episode = None
+
     output_dir = root_output_dir / f"S{season:02}E{episode:02}.subplant"
     if output_dir.is_dir():
         shutil.rmtree(str(output_dir))
     output_dir.mkdir(parents=True, exist_ok=False)
 
-    # Get subtitle tracks, build map {Path => (file_name, SubtitleMetadata)}
-    # TODO: better file name uniqueness if multiple subs have the same language
+    # Get subtitle tracks
     sub_map = {}
+    sub_extract_args = []
     for sub_track in mkv.tracks:
         if sub_track.track_type != "subtitles":
             continue
@@ -50,15 +54,24 @@ def process(mkv_path: Path, root_output_dir: Path) -> None:
             forced=sub_track.forced_track or False,
         )
         sub_extension = SUBTITLE_CODEC_EXTENSIONS.get(sub_track.track_codec, "")
-        extracted_sub_path = output_dir / f"{sub_metadata.lang}{sub_extension}"
-        print(f"Extracting {extracted_sub_path.relative_to(root_output_dir)}")
-        # TODO: group these and do them in one command execution
-        mkvextract_tracks(
-            mkv_path,
-            sub_track.track_id,
-            extracted_sub_path,
+        extracted_sub_path = (
+            output_dir
+            / f"{sub_track.track_id}-{sub_metadata.lang}{sub_extension}"
         )
-        sub_map[extracted_sub_path] = (extracted_sub_path.name, sub_metadata)
+        sub_map[extracted_sub_path.name] = sub_metadata
+        sub_extract_args.append(f"{sub_track.track_id}:{extracted_sub_path}")
+    
+    # Extract subtitles
+    print(f"Extracting {len(sub_map)} subtitle(s)")
+    subprocess.check_call(
+        [
+            "mkvextract",
+            "tracks",
+            "--quiet",
+            str(mkv_path),
+            *sub_extract_args,
+        ]
+    )
 
     # Extract attachments
     attachments_dir = output_dir / "attachments"
@@ -74,22 +87,10 @@ def process(mkv_path: Path, root_output_dir: Path) -> None:
     )
 
     # Build VideoMetadata
-    video_metadata = VideoMetadata(season, episode, subs=dict(sub_map.values()))
+    video_metadata = VideoMetadata(season, episode, sub_map)
     metadata_file = output_dir / METADATA_FILE_NAME
     metadata_file.write_text(pyron.to_string(video_metadata) + "\n")
-
-
-def mkvextract_tracks(mkv_path: Path, track_id: int, destination: Path) -> None:
-    subprocess.check_call(
-        [
-            "mkvextract",
-            "tracks",
-            "--quiet",
-            str(mkv_path),
-            f"{track_id}:{destination}",
-        ]
-    )
-
+    
 
 def get_attachments(mkv_path: Path) -> list[AttachmentMetadata]:
     json_info = subprocess.check_output(["mkvmerge", "-J", mkv_path])
