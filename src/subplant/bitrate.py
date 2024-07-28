aacc86429281d0aaf8abe52b24a39007a28adde7
alpha-tango-kilo <git@heyatk.com>
2024-07-28 19:51:17 +0100

    Support folders; convert to kb/s

--- src/subplant/bitrate.py
+++ src/subplant/bitrate.py
@@ -9,18 +9,24 @@ class BitrateArgs(Protocol):
 
 
 def process_one(file: Path) -> None:
+    def to_kbps(bps: int) -> int:
+        return bps // 1024
+
     media = MediaInfo.parse(file)
     assert isinstance(media, MediaInfo)
 
-    # TODO: convert bits per second into something human readable
     print(f"{file.name}:")
-    for index, track in enumerate(media.video_tracks):
+    for index, track in enumerate(media.video_tracks, start=1):
+        assert isinstance(track.bit_rate, int)
         print(
-            f"  Video #{index}: {track.bit_rate} b/s ({track.bit_depth}-bit colour)"
+            f"  Video #{index}:     "
+            f"{to_kbps(track.bit_rate)} kb/s ({track.bit_depth}-bit colour)"
         )
-    for index, track in enumerate(media.audio_tracks):
+    for index, track in enumerate(media.audio_tracks, start=1):
+        assert isinstance(track.bit_rate, int)
         print(
-            f"  Audio #{index} ({track.language}): {track.bit_rate} b/s {track.format}"
+            f"  Audio #{index} ({track.language}): "
+            f"{to_kbps(track.bit_rate)} kb/s {track.format}"
         )
 
 
@@ -28,6 +34,8 @@ def bitrate(args: BitrateArgs) -> None:
     if args.work_path.is_file():
         process_one(args.work_path)
     elif args.work_path.is_dir():
-        print("unsupported")
+        # Note: pymediainfo does support other extensions, but .glob() doesn't expand {}
+        for file in sorted(args.work_path.glob("*.mkv")):
+            process_one(file)
     else:
         raise ValueError("work_path must be to file or folder")
